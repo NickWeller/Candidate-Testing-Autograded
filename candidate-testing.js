fd615961dc7db00b6f15646492a1e6f4ffcfc550
NickWeller <nickweller2@gmail.com>
2024-05-18 11:59:04 -0500

    Assignment 1: Part 2

--- candidate-testing.js
+++ candidate-testing.js
@@ -7,13 +7,16 @@ let candidateName = " ";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
-let candidateAnswer = " ";
-
-
+let candidateAnswer = "";
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = ["Who was the first American woman in space? ", 
+"True or false: 5 kilometer == 5000 meters? ",
+"(5 + 3)/2 * 10 = ? ",
+"Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+"What is the minimum crew size for the ISS? "];
+
+let correctAnswers = ["Sally Ride", "true", "40","Trajectory", "3"];
+let candidateAnswers = [];
 
 
 function askForName() {
@@ -23,18 +26,39 @@ function askForName() {
 
 function askQuestion() {
 // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer // 
-candidateAnswer = input.question("Who was the first American woman in space? ")
+for (let i = 0; i < question.length; i++){
+  let stagingAnswer = input.question(questions[i]);
+  candidateAnswer.push(stagingAnswer)
 }
-askForName()
-askQuestion()
+}
 
-console.log(candidateName)
-console.log(candidateAnswer)
+
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
-
+  console.log
+  (`
+  Candidate Name: ${candidateName} 
+  1) ${questions[0]}
+  Your answer: ${candidateAnswers[0]}
+  Correct Answer: ${correctAnswers[0]}
+  
+  2) ${questions[1]}
+  Your Answer: ${candidateAnswers[1]}
+  Correct Answer: ${correctAnswers[1]}
+  
+  3) ${questions[2]}
+  Your Answer: ${candidateAnswers[2]}
+  Correct Answer: ${correctAnswers[2]}
+  
+  4) ${questions[3]}
+  Your Answer: ${candidateAnswers[3]}
+  Correct Answer: ${correctAnswers[3]}
+  
+  5) ${questions[4]}
+  Your Answer: ${candidateAnswers[4]}
+  Correct Answer: ${correctAnswers[4]}`);
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
 
@@ -45,7 +69,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-   console.log("Hello " + candidateName);
+   console.log("Hello  ${candidateName} Welcome to the quiz");
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
