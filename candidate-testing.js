7b9410813bdf0d2cb8a9897b355465eb3236dfdb
NickWeller <nickweller2@gmail.com>
2024-05-18 16:20:37 -0500

    Assignment 1 FINAL

--- candidate-testing.js
+++ candidate-testing.js
@@ -26,9 +26,8 @@ function askForName() {
 
 function askQuestion() {
 // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer // 
-for (let i = 0; i < question.length; i++){
-  let stagingAnswer = input.question(questions[i]);
-  candidateAnswer.push(stagingAnswer)
+for (let i = 0; i < questions.length; i++){
+  candidateAnswers.push(input.question(questions[i]));
 }
 }
 
@@ -41,35 +40,45 @@ function gradeQuiz(candidateAnswers) {
   (`
   Candidate Name: ${candidateName} 
   1) ${questions[0]}
-  Your answer: ${candidateAnswers[0]}
+  Candidate Answer: ${candidateAnswers[0]}
   Correct Answer: ${correctAnswers[0]}
   
   2) ${questions[1]}
-  Your Answer: ${candidateAnswers[1]}
+  Candidate Answer: ${candidateAnswers[1]}
   Correct Answer: ${correctAnswers[1]}
   
   3) ${questions[2]}
-  Your Answer: ${candidateAnswers[2]}
+  Candidate Answer: ${candidateAnswers[2]}
   Correct Answer: ${correctAnswers[2]}
   
   4) ${questions[3]}
-  Your Answer: ${candidateAnswers[3]}
+  Candidate Answer: ${candidateAnswers[3]}
   Correct Answer: ${correctAnswers[3]}
   
   5) ${questions[4]}
-  Your Answer: ${candidateAnswers[4]}
+  Candidate Answer: ${candidateAnswers[4]}
   Correct Answer: ${correctAnswers[4]}`);
 
-  let grade;  //TODO 3.2 use this variable to calculate the candidates score.
-
-
-  return grade;
+let correctCount = 0
+let grade = 0;  //TODO 3.2 use this variable to calculate the candidates score.
+for (let i = 0; i < correctAnswers.length; i++){
+  if (correctAnswers[i].toLowerCase() === candidateAnswers[i].toLowerCase()){
+  correctCount = correctCount + 1}
+}
+  grade = correctCount/correctAnswers.length *100
+  let result = ''
+  if (grade >= 80){
+    result = "passed"
+  }else{
+    result = "failed"
+  }
+  return console.log(`\nYou have ${result} the quiz with a grade of ${grade}%`);
 }
 
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-   console.log("Hello  ${candidateName} Welcome to the quiz");
+   console.log(`Hello ${candidateName}! Welcome to Candidate Testing`);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
