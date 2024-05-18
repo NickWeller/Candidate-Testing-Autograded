4747fd2f6de4c4dfabae2fd5ca6c2e9df7729f40
NickWeller <nickweller2@gmail.com>
2024-05-18 17:37:58 -0500

    FINAL

--- candidate-testing.js
+++ candidate-testing.js
@@ -87,7 +87,6 @@ function runProgram() {
 }
 
 // Don't write any code below this line //
-// And don't change these or your program will not run as expected //
 module.exports = {
   candidateName: candidateName,
   question: question,
